7cb4f1bc319776ba39b37d953a9853c677bb1ca2
veniamin <veniamin_petrikovskyi@outlook.com>
2024-05-15 21:58:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.reply.ui
+
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.reply.data.Email
+import com.example.reply.data.MailboxType
+import com.example.reply.ui.utils.ReplyNavigationType
+
+@Composable
+fun ReplyApp(
+    windowSize: WindowWidthSizeClass,
+    modifier: Modifier = Modifier,
+) {
+    val viewModel: ReplyViewModel = viewModel()
+    val replyUiState = viewModel.uiState.collectAsState().value
+
+    val replyNavigationType: ReplyNavigationType = when (windowSize) {
+        WindowWidthSizeClass.Compact -> {
+            ReplyNavigationType.BOTTOM_NAVIGATION
+        }
+        WindowWidthSizeClass.Medium -> {
+            ReplyNavigationType.NAVIGATION_RAIL
+        }
+        WindowWidthSizeClass.Expanded -> {
+            ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
+        }
+        else -> {
+            ReplyNavigationType.BOTTOM_NAVIGATION
+        }
+    }
+
+    ReplyHomeScreen(
+        navigationType = replyNavigationType,
+        replyUiState = replyUiState,
+        onTabPressed = { mailboxType: MailboxType ->
+            viewModel.updateCurrentMailbox(mailboxType = mailboxType)
+            viewModel.resetHomeScreenStates()
+        },
+        onEmailCardPressed = { email: Email ->
+            viewModel.updateDetailsScreenStates(
+                email = email
+            )
+        },
+        onDetailScreenBackPressed = {
+            viewModel.resetHomeScreenStates()
+        },
+        modifier = modifier
+    )
+}
